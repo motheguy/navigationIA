e2e28d78e71b1d179b545ecae0d7ca42d60fd15d
MarineCrlz <marine.carliez@gmail.com>
2020-11-24 08:34:51 +0100

    Ajout affichage temps

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -44,23 +44,27 @@
             this.cmbBxWind = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.textBoxTime = new System.Windows.Forms.TextBox();
+            this.labelTime = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOcean)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBoxOcean
             // 
-            this.pictureBoxOcean.Location = new System.Drawing.Point(243, 28);
+            this.pictureBoxOcean.Location = new System.Drawing.Point(304, 35);
+            this.pictureBoxOcean.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBoxOcean.Name = "pictureBoxOcean";
-            this.pictureBoxOcean.Size = new System.Drawing.Size(300, 300);
+            this.pictureBoxOcean.Size = new System.Drawing.Size(375, 375);
             this.pictureBoxOcean.TabIndex = 0;
             this.pictureBoxOcean.TabStop = false;
             // 
             // btnClic
             // 
             this.btnClic.AccessibleName = "";
-            this.btnClic.Location = new System.Drawing.Point(12, 12);
+            this.btnClic.Location = new System.Drawing.Point(15, 15);
+            this.btnClic.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnClic.Name = "btnClic";
-            this.btnClic.Size = new System.Drawing.Size(94, 29);
+            this.btnClic.Size = new System.Drawing.Size(118, 36);
             this.btnClic.TabIndex = 1;
             this.btnClic.Text = "Demarrer";
             this.btnClic.UseVisualStyleBackColor = true;
@@ -68,10 +72,11 @@
             // 
             // textBoxXi
             // 
-            this.textBoxXi.Location = new System.Drawing.Point(58, 160);
+            this.textBoxXi.Location = new System.Drawing.Point(72, 200);
+            this.textBoxXi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxXi.Name = "textBoxXi";
             this.textBoxXi.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.textBoxXi.Size = new System.Drawing.Size(130, 27);
+            this.textBoxXi.Size = new System.Drawing.Size(162, 31);
             this.textBoxXi.TabIndex = 2;
             this.textBoxXi.Text = "3";
             this.textBoxXi.TextChanged += new System.EventHandler(this.NewCoordinate);
@@ -79,27 +84,30 @@
             // lblInitCordinates
             // 
             this.lblInitCordinates.AutoSize = true;
-            this.lblInitCordinates.Location = new System.Drawing.Point(12, 126);
+            this.lblInitCordinates.Location = new System.Drawing.Point(15, 158);
+            this.lblInitCordinates.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblInitCordinates.Name = "lblInitCordinates";
-            this.lblInitCordinates.Size = new System.Drawing.Size(163, 20);
+            this.lblInitCordinates.Size = new System.Drawing.Size(196, 25);
             this.lblInitCordinates.TabIndex = 3;
             this.lblInitCordinates.Text = "Coordonnées initiales : ";
             // 
             // lblXi
             // 
             this.lblXi.AutoSize = true;
-            this.lblXi.Location = new System.Drawing.Point(23, 162);
+            this.lblXi.Location = new System.Drawing.Point(29, 202);
+            this.lblXi.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblXi.Name = "lblXi";
-            this.lblXi.Size = new System.Drawing.Size(29, 20);
+            this.lblXi.Size = new System.Drawing.Size(36, 25);
             this.lblXi.TabIndex = 4;
             this.lblXi.Text = "Xi :";
             // 
             // textBoxYi
             // 
-            this.textBoxYi.Location = new System.Drawing.Point(58, 206);
+            this.textBoxYi.Location = new System.Drawing.Point(72, 258);
+            this.textBoxYi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxYi.Name = "textBoxYi";
             this.textBoxYi.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.textBoxYi.Size = new System.Drawing.Size(130, 27);
+            this.textBoxYi.Size = new System.Drawing.Size(162, 31);
             this.textBoxYi.TabIndex = 5;
             this.textBoxYi.Text = "3";
             this.textBoxYi.TextChanged += new System.EventHandler(this.NewCoordinate);
@@ -107,55 +115,61 @@
             // lblYi
             // 
             this.lblYi.AutoSize = true;
-            this.lblYi.Location = new System.Drawing.Point(23, 206);
+            this.lblYi.Location = new System.Drawing.Point(29, 258);
+            this.lblYi.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblYi.Name = "lblYi";
-            this.lblYi.Size = new System.Drawing.Size(28, 20);
+            this.lblYi.Size = new System.Drawing.Size(35, 25);
             this.lblYi.TabIndex = 6;
             this.lblYi.Text = "Yi :";
             // 
             // lblEndCordinates
             // 
             this.lblEndCordinates.AutoSize = true;
-            this.lblEndCordinates.Location = new System.Drawing.Point(12, 254);
+            this.lblEndCordinates.Location = new System.Drawing.Point(15, 318);
+            this.lblEndCordinates.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblEndCordinates.Name = "lblEndCordinates";
-            this.lblEndCordinates.Size = new System.Drawing.Size(151, 20);
+            this.lblEndCordinates.Size = new System.Drawing.Size(183, 25);
             this.lblEndCordinates.TabIndex = 7;
             this.lblEndCordinates.Text = "Coordonnées finales :";
             // 
             // lblXf
             // 
             this.lblXf.AutoSize = true;
-            this.lblXf.Location = new System.Drawing.Point(21, 286);
+            this.lblXf.Location = new System.Drawing.Point(26, 358);
+            this.lblXf.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblXf.Name = "lblXf";
-            this.lblXf.Size = new System.Drawing.Size(30, 20);
+            this.lblXf.Size = new System.Drawing.Size(38, 25);
             this.lblXf.TabIndex = 8;
             this.lblXf.Text = "Xf :";
             // 
             // lblYf
             // 
             this.lblYf.AutoSize = true;
-            this.lblYf.Location = new System.Drawing.Point(23, 322);
+            this.lblYf.Location = new System.Drawing.Point(29, 402);
+            this.lblYf.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblYf.Name = "lblYf";
-            this.lblYf.Size = new System.Drawing.Size(29, 20);
+            this.lblYf.Size = new System.Drawing.Size(37, 25);
             this.lblYf.TabIndex = 10;
             this.lblYf.Text = "Yf :";
             // 
             // textBoxXf
             // 
-            this.textBoxXf.Location = new System.Drawing.Point(58, 286);
+            this.textBoxXf.Location = new System.Drawing.Point(72, 358);
+            this.textBoxXf.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxXf.Name = "textBoxXf";
             this.textBoxXf.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.textBoxXf.Size = new System.Drawing.Size(130, 27);
+            this.textBoxXf.Size = new System.Drawing.Size(162, 31);
             this.textBoxXf.TabIndex = 11;
             this.textBoxXf.Text = "100";
             this.textBoxXf.TextChanged += new System.EventHandler(this.NewCoordinate);
             // 
             // textBoxYf
             // 
-            this.textBoxYf.Location = new System.Drawing.Point(58, 322);
+            this.textBoxYf.Location = new System.Drawing.Point(72, 402);
+            this.textBoxYf.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxYf.Name = "textBoxYf";
             this.textBoxYf.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.textBoxYf.Size = new System.Drawing.Size(130, 27);
+            this.textBoxYf.Size = new System.Drawing.Size(162, 31);
             this.textBoxYf.TabIndex = 12;
             this.textBoxYf.Text = "100";
             this.textBoxYf.TextChanged += new System.EventHandler(this.NewCoordinate);
@@ -163,9 +177,10 @@
             // lblWind
             // 
             this.lblWind.AutoSize = true;
-            this.lblWind.Location = new System.Drawing.Point(13, 60);
+            this.lblWind.Location = new System.Drawing.Point(16, 75);
+            this.lblWind.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblWind.Name = "lblWind";
-            this.lblWind.Size = new System.Drawing.Size(100, 20);
+            this.lblWind.Size = new System.Drawing.Size(122, 25);
             this.lblWind.TabIndex = 14;
             this.lblWind.Text = "Type de vent :";
             // 
@@ -176,18 +191,20 @@
             "a",
             "b",
             "c"});
-            this.cmbBxWind.Location = new System.Drawing.Point(58, 86);
+            this.cmbBxWind.Location = new System.Drawing.Point(72, 108);
+            this.cmbBxWind.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmbBxWind.Name = "cmbBxWind";
             this.cmbBxWind.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.cmbBxWind.Size = new System.Drawing.Size(130, 28);
+            this.cmbBxWind.Size = new System.Drawing.Size(162, 33);
             this.cmbBxWind.TabIndex = 15;
             this.cmbBxWind.Text = "a";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(123, 12);
+            this.button1.Location = new System.Drawing.Point(154, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.Size = new System.Drawing.Size(118, 36);
             this.button1.TabIndex = 16;
             this.button1.Text = "Test Animation";
             this.button1.UseVisualStyleBackColor = true;
@@ -195,16 +212,35 @@
             // 
             // textBoxResult
             // 
-            this.textBoxResult.Location = new System.Drawing.Point(240, 363);
+            this.textBoxResult.Location = new System.Drawing.Point(304, 418);
+            this.textBoxResult.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxResult.Name = "textBoxResult";
-            this.textBoxResult.Size = new System.Drawing.Size(303, 27);
+            this.textBoxResult.Size = new System.Drawing.Size(378, 31);
             this.textBoxResult.TabIndex = 17;
+            // 
+            // textBoxTime
+            // 
+            this.textBoxTime.Location = new System.Drawing.Point(452, 456);
+            this.textBoxTime.Name = "textBoxTime";
+            this.textBoxTime.Size = new System.Drawing.Size(230, 31);
+            this.textBoxTime.TabIndex = 18;
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(304, 459);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(142, 25);
+            this.labelTime.TabIndex = 19;
+            this.labelTime.Text = "Temps de trajet :";
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(570, 401);
+            this.ClientSize = new System.Drawing.Size(712, 501);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.textBoxTime);
             this.Controls.Add(this.textBoxResult);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.cmbBxWind);
@@ -222,6 +258,7 @@
             this.Controls.Add(this.btnClic);
             this.Controls.Add(this.pictureBoxOcean);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainForm";
             this.Text = "Navigation";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOcean)).EndInit();
@@ -248,6 +285,8 @@
         private System.Windows.Forms.ComboBox cmbBxWind;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.TextBox textBoxTime;
+        private System.Windows.Forms.Label labelTime;
     }
 }
 
